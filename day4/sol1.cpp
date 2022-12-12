fa4e45c55e843abc36f17f8e6007247a31558c8a
filbert <fillberth@gmail.com>
2022-12-12 20:19:48 +0800

    day 4

---
+++ day4/sol1.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int count = 0;
+    string pair_string;
+    while (cin >> pair_string)
+    {
+        // get start and end ranges of each pair
+        char *first_range = strtok(&pair_string[0], ",");
+        char *second_range = strtok(NULL, ",");
+        int first_start = stoi(strtok(first_range, "-"));
+        int first_end = stoi(strtok(NULL, "-"));
+        int second_start = stoi(strtok(second_range, "-"));
+        int second_end = stoi(strtok(NULL, "-"));
+
+        if (first_start < second_start)
+        {
+            if (first_end >= second_end)
+            {
+                count += 1;
+            }
+        }
+        else if (first_start > second_start)
+        {
+            if (first_end <= second_end)
+            {
+                count += 1;
+            }
+        }
+        else
+        {
+            count += 1;
+        }
+    }
+    cout << count << endl;
+}

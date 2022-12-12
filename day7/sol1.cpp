81b9ac7895ecbabd57ac9a633b43e274d0d8ee5a
filbert <fillberth@gmail.com>
2022-12-12 22:27:33 +0800

    day 7: create tree

---
+++ day7/sol1.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct treeNode
+{
+    map<string, treeNode *> children;
+    treeNode *parent;
+    string name = "";
+    long size = 0;
+};
+
+int main()
+{
+    // init tree
+    string command;
+    string current_dir;
+    treeNode root;
+    root.name = "/";
+    treeNode *current = &root;
+
+    // parse directory tree
+    while (cin >> command)
+    {
+        // check for cd and ls commands
+        if (command == "$")
+        {
+            cin >> command;
+            // cd
+            if (command == "cd")
+            {
+                cin >> current_dir;
+                if (current_dir == "/")
+                {
+                }
+                // traverse back up to parent
+                else if (current_dir == "..")
+                {
+                    current = current->parent;
+                }
+                // set current to the correct child
+                else
+                {
+                    current = current->children[current_dir];
+                }
+            }
+            // nothing needed if command is ls
+        }
+        // check for directories
+        else if (command == "dir")
+        {
+            string name;
+            cin >> name;
+
+            // create a new treeNode corresponding to that directory
+            treeNode *tmp = new treeNode();
+            tmp->name = name;
+            tmp->parent = current;
+
+            // add it as a child of the current directory
+            current->children[name] = tmp;
+        }
+        // check for files
+        else
+        {
+            // add file size to total size of current directory
+            long size = stol(command);
+            current->size += size;
+            cin >> command;
+        }
+    }
+
+    cout << root.size << endl;
+}

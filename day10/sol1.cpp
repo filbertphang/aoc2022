6622d338585ca9d1d3c319904b8123316e2f256c
filbert <fillberth@gmail.com>
2022-12-13 11:58:08 +0800

    day 10: finish part 1

---
+++ day10/sol1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int value = 1;
+    int target = 20;
+    int sum = 0;
+
+    string command;
+    int signal;
+    bool skip = false;
+    for (int current = 1; current <= 220; current++)
+    {
+        if (current == target)
+        {
+            sum += current * value;
+            target += 40;
+            cout << "cycle " << current << ": " << value << endl;
+        }
+        if (!skip)
+        {
+            cin >> command;
+            if (command == "addx")
+            {
+                cin >> signal;
+                skip = true;
+            }
+        }
+        else
+        {
+            value += signal;
+            skip = false;
+        }
+    }
+    cout << sum << endl;
+}

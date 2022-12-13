ca6eb22a9765b6f2ed448ff940fa177d0a750284
filbert <fillberth@gmail.com>
2022-12-13 14:45:11 +0800

    day 11: finish part 1

--- day11/sol1.cpp
+++ day11/sol1.cpp
@@ -3,19 +3,61 @@ using namespace std;
 
 struct monkey
 {
-    vector<int> items;
+    list<int> items;
     char operation;
     string operand;
     int test;
     int if_true;
     int if_false;
+    int inspections = 0;
 };
+
+int inspect(monkey *monkey, int *item)
+{
+    monkey->inspections += 1;
+    int worry = *item;
+    int op;
+    if (monkey->operand == "old")
+    {
+        op = *item;
+    }
+    else
+    {
+        op = stoi(monkey->operand);
+    }
+
+    // calculate worry from inspection
+    if (monkey->operation == '+')
+    {
+        worry += op;
+    }
+    else
+    {
+        worry *= op;
+    }
+
+    // relax after item inspection
+    // update new worry value of item
+    worry = worry / 3;
+    *item = worry;
+
+    // decide which monkey to throw to
+    if (worry % monkey->test == 0)
+    {
+        return monkey->if_true;
+    }
+    else
+    {
+        return monkey->if_false;
+    }
+}
 
 int main()
 {
-    monkey monkeys[8];
+    const int N = 8;
+    monkey monkeys[N];
     // string parsing in c++ is a nightmare
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < N; i++)
     {
         // create monkey
         monkey *cur = new monkey();
@@ -31,7 +73,7 @@ int main()
         getline(cin, line);
         strtok(&line[0], ":");
         char *item_str = strtok(NULL, ":");
-        vector<int> *items = new vector<int>();
+        list<int> *items = new list<int>();
         char *tok_result = strtok(item_str, ",");
         int item_id;
         while (tok_result != NULL)
@@ -86,7 +128,55 @@ int main()
         // store monkey and read blank line
         monkeys[i] = *cur;
         getline(cin, line);
-
-        cout << monkeys[i].operation << " " << monkeys[i].operand << " " << monkeys[i].test << " " << monkeys[i].if_true << " " << monkeys[i].if_false << endl;
     }
+
+    // repeat for 20 steps
+    for (int j = 0; j < 20; j++)
+    {
+        // handle behaviour for each monkey
+        for (int m = 0; m < N; m++)
+        {
+            // throw items until that monkey has no items left
+            while (!monkeys[m].items.empty())
+            {
+                // get top item
+                int item = monkeys[m].items.front();
+                monkeys[m].items.pop_front();
+
+                // inspect top item
+                int target = inspect(&monkeys[m], &item);
+
+                // throw top item to next monkey
+                monkeys[target].items.push_back(item);
+            }
+        }
+
+        // debug print statements
+        /*
+        cout << "===== after round " << j << " =====" << endl;
+        for (int m = 0; m < N; m++)
+        {
+            ;
+            cout << m << ": ";
+            for (int it : monkeys[m].items)
+            {
+                cout << it << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        */
+    }
+
+    // count number of inspections
+    int inspections[N];
+    for (int k = 0; k < N; k++)
+    {
+        inspections[k] = monkeys[k].inspections;
+    }
+    sort(inspections, inspections + N);
+
+    // compute monkey business... heh
+    int monkey_business = inspections[N - 2] * inspections[N - 1];
+    cout << monkey_business << endl;
 }

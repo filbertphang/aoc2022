9ca6a6cf866ae82f716ad25ac7f530617df4652e
filbert <fillberth@gmail.com>
2022-12-14 15:46:57 +0800

    day 14: finish part 1

---
+++ day14/sol1.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ymax = 0;
+const int LINES = 155;
+map<int, map<int, bool>> grid; // x, then y
+
+// returns:
+// - 0, if this sand cannot be dropped
+// - 1, if the sand ends in a stable position
+// - 2, if the sand can fall freely
+int drop_sand(int start_x, int start_y)
+{
+    // some obstruction within the same column
+    if (grid.find(start_x) != grid.end())
+    {
+        map<int, bool> *col = &grid[start_x];
+        map<int, bool>::iterator obs_it = col->lower_bound(start_y);
+        // no obstruction below the sand's current position
+        // sand can fall freely
+        if (obs_it == col->end())
+        {
+            return 2;
+        }
+        // obstruction is at the sand's current position
+        // sand cannot be dropped
+        else if (obs_it->first == start_y)
+        {
+            return 0;
+        }
+        // some obstruction below the sand's current position
+        // drop to diagonal left, then diagonal right
+        else
+        {
+            int next_y = obs_it->first;
+            // try drop diagonally left first
+            int diag_left = drop_sand(start_x - 1, next_y);
+            if (diag_left != 0)
+            {
+                return diag_left;
+            }
+            else
+            {
+                // try dropping diagonally right next
+                int diag_right = drop_sand(start_x + 1, next_y);
+                if (diag_right != 0)
+                {
+                    return diag_right;
+                }
+                // cannot drop either diagonals
+                // set send to rest at current spot
+                else
+                {
+                    grid[start_x][next_y - 1] = true;
+                    return 1;
+                }
+            }
+        }
+    }
+    // no obstructions in the same column, sand can fall freely
+    else
+    {
+        return 2;
+    }
+}
+
+int main()
+{
+    string line;
+    regex num("[0-9]+");
+    regex_token_iterator<string::iterator> rend;
+    for (int z = 0; z < LINES; z++)
+    {
+        // parse rock formations
+        getline(cin, line);
+        regex_token_iterator<string::iterator> matches(line.begin(), line.end(), num);
+        int x1, y1;
+        int x2 = stoi(*matches++);
+        int y2 = stoi(*matches++);
+        ymax = max(ymax, y2);
+        while (matches != rend)
+        {
+            x1 = x2;
+            y1 = y2;
+            x2 = stoi(*matches++);
+            y2 = stoi(*matches++);
+            ymax = max(ymax, y2);
+            if (x1 == x2)
+            {
+                for (int i = min(y1, y2); i <= max(y1, y2); i++)
+                {
+                    grid[x1][i] = true;
+                }
+            }
+            else if (y1 == y2)
+            {
+                for (int i = min(x1, x2); i <= max(x1, x2); i++)
+                {
+                    grid[i][y1] = true;
+                }
+            }
+        }
+    }
+
+    int count = 1;
+    int res = drop_sand(500, 0);
+    while (res != 2)
+    {
+        res = drop_sand(500, 0);
+        count += 1;
+    }
+    cout << count - 1 << endl;
+}

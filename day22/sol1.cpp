c2a75932077fef1c3017e8134d008aa7fc223bb2
filbert <fillberth@gmail.com>
2022-12-26 12:44:20 +0800

    day 22: parse input properly

--- day22/sol1.cpp
+++ day22/sol1.cpp
@@ -1,45 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct row
-{
-    int start;
-    int end;
-    vector<int> walls;
-};
-
 int main()
 {
-    vector<row> rows;
+    map<int, map<int, char>> walls; // column, then row
+    vector<pair<int, int>> rows;
+    vector<pair<int, int>> cols;
 
-    // parse grove map
+    // parse grove map following the rows
     string line;
+    int row_n = 0;
     getline(cin, line);
-    while (!cin.eof())
+    while (line.size() > 0)
     {
+        cout << line << " " << line.size() << endl;
         bool started = false;
-        row *cur_row = new row();
+        pair<int, int> *cur_row = new pair<int, int>();
+
         for (int i = 0; i < line.size(); i++)
         {
+            // get starting position of row
             if (line[i] == '.' || line[i] == '#')
             {
-                // note down the starting position of that row
                 if (!started)
                 {
                     started = true;
-                    cur_row->start = i;
+                    cur_row->first = i;
+                }
+
+                // store walls position
+                if (walls.count(i) == 1)
+                {
+                    walls[i][row_n] = line[i];
+                }
+                else
+                {
+                    map<int, char> *cur_col_walls = new map<int, char>();
+                    (*cur_col_walls)[row_n] = line[i];
+                    walls[i] = *cur_col_walls;
                 }
             }
-            if (line[i] == '#')
-            {
-                // add wall to walls list
-                cur_row->walls.push_back(i);
-            }
         }
-        cur_row->end = line.size() - 1;
+        cur_row->second = line.size() - 1;
 
         rows.push_back(*cur_row);
+        row_n += 1;
         getline(cin, line);
+    }
+
+    // get column data from grove map
+    for (pair<int, map<int, char>> col : walls)
+    {
+        int start = col.second.begin()->first;
+        int end = col.second.rbegin()->first;
+        pair<int, int> *cur_col = new pair<int, int>();
+        cur_col->first = start;
+        cur_col->second = end;
+        cols.push_back(*cur_col);
     }
 
     // parse directions from monkeys
